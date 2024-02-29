ac1d1adfdcf1a2609c80797e137f88a4873e09dc
Kamil Śliwak <kamil.sliwak@codepoets.it>
2024-02-29 17:14:19 +0100

    More contracts and calls: FixedFeeRegistrar, ramanujan_pi, prbmath_unsigned, strings

--- download-sources.sh
+++ download-sources.sh
@@ -8,3 +8,6 @@ rm -rf solidity/
 git clone https://github.com/ethereum/solidity/ --branch v0.8.24 --depth 1
 find solidity/ -type f,l      -not -path 'solidity/test/libsolidity/semanticTests/externalContracts/*' -delete
 find solidity/ -type d -empty -not -path 'solidity/test/libsolidity/semanticTests/externalContracts'   -delete
+sed -i -e '/^==== .* ====$/d' solidity/test/libsolidity/semanticTests/externalContracts/strings.sol
+sed -i -e '/^==== .* ====$/d' solidity/test/libsolidity/semanticTests/externalContracts/ramanujan_pi.sol
+sed -i -e '/^==== .* ====$/d' solidity/test/libsolidity/semanticTests/externalContracts/prbmath_unsigned.sol
