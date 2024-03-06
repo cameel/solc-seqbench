2b3e37c2633ef38cecbf2184a8621012adbda5a2
Kamil Śliwak <kamil.sliwak@codepoets.it>
2024-03-06 18:50:15 +0100

    New input contract: ERC20 from solc's semanticTests

--- download-sources.sh
+++ download-sources.sh
@@ -6,8 +6,8 @@ cd input/sources/
 
 rm -rf solidity/
 git clone https://github.com/ethereum/solidity/ --branch v0.8.24 --depth 1
-find solidity/ -type f,l      -not -path 'solidity/test/libsolidity/semanticTests/externalContracts/*' -delete
-find solidity/ -type d -empty -not -path 'solidity/test/libsolidity/semanticTests/externalContracts'   -delete
+find solidity/ -type f,l      -not -path 'solidity/test/libsolidity/semanticTests/*' -delete
+find solidity/ -type d -empty -not -path 'solidity/test/libsolidity/semanticTests'   -delete
 sed -i -e '/^==== .* ====$/d' solidity/test/libsolidity/semanticTests/externalContracts/strings.sol
 sed -i -e '/^==== .* ====$/d' solidity/test/libsolidity/semanticTests/externalContracts/ramanujan_pi.sol
 sed -i -e '/^==== .* ====$/d' solidity/test/libsolidity/semanticTests/externalContracts/prbmath_unsigned.sol
